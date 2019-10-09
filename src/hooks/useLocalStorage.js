58c7f33e025ca4e1ddd4d87a198a2b3bbaa74178
Carlos <miklo.concepcion@gmail.com>
2019-10-09 18:58:22 -0500

    finishing up dark mode MVP requirement. Broken at the moment :)

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,14 +5,14 @@ import { useState } from "react";
 // To retrieve an item from localStorage, call localStorage.getItem('itemName')
 // If that item doesn't exist, it will return undefined
 const [storedValue, setStoredValue] = useState(() => {
-    // Get from local storage by key
-    const item = window.localStorage.getItem(key);
-    // Parse and return stored json or, if undefined, return initialValue
-    return item ? JSON.parse(item) : initialValue;
-  });// const [name, setName] = useLocalStorage('name', 'Carlitos')
+  // Get from local storage by key
+  const item = window.localStorage.getItem(key);
+  // Parse and return stored json or, if undefined, return initialValue
+  return item ? JSON.parse(item) : initialValue;
+
+});
 
 
-  
 export const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
       const item = window.localStorage.getItem(key);
@@ -21,3 +21,4 @@ export const useLocalStorage = (key, initialValue) => {
   
     return [storedValue];
   };
+  
